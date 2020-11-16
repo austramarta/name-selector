4b02080dcfccb8418def364a1a13c23a4ce89f07
austramarta <austramarta@users.noreply.github.com>
2020-11-16 18:02:37 +0200

    some temporary minimal styling

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const selectButton = document.getElementById("select-name")
 
 
 function enterNames() {
-    //split all the names by commas
-    let names = nameInput.value.split(",");
+    //clear names div before adding new names
     allNames.innerHTML = "";
 
+    //split all the names by commas
+    let names = nameInput.value.split(",");
 
+    //display all names below the button by creating new "name" divs
     for (name of names) {
         const newName = document.createElement("div");
         newName.classList.add("name");
         newName.innerText = name;
         allNames.appendChild(newName);
+        nameInput.value = "";
     }
 
 
@@ -21,8 +24,6 @@ function enterNames() {
 
 
 nameInput.addEventListener("keyup", function (event) {
-
-
     if (event.keyCode === 13) {
         event.preventDefault();
         selectButton.click();
