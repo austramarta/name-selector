87bfdae06ffd52ad09eee5e4dd6b9fe6382baa1c
austramarta <austramarta@users.noreply.github.com>
2020-11-16 15:49:45 +0200

    js for displaying names

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-let names = [];
+const nameInput = document.getElementById("name-input");
+const allNames = document.getElementById("name");
+const selectButton = document.getElementById("select-name")
+
+
+function enterNames() {
+    //split all the names by commas
+    let names = nameInput.value.split(",");
+    allNames.innerHTML = "";
+
+
+    for (name of names) {
+        const newName = document.createElement("div");
+        newName.classList.add("name");
+        newName.innerText = name;
+        allNames.appendChild(newName);
+    }
+
+
+}
+
+
+nameInput.addEventListener("keyup", function (event) {
+
+
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        selectButton.click();
+    }
+});
