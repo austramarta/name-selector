23b7d8346d08613eb4941f4b9a5b22c2582a2d1c
austramarta <austramarta@users.noreply.github.com>
2020-11-24 21:52:24 +0200

    finished functionality

--- main.js
+++ main.js
@@ -1,44 +1,36 @@
-const nameInput = document.getElementById("name-input");
-const allNames = document.getElementById("name");
-const selectButton = document.getElementById("select-name")
-// let savedNames = [];
+const nameInputField = document.getElementById("name-input");
+const nameDiv = document.getElementById("name-wrapper");
+const selectButton = document.getElementById("select-name");
 
 
 function enterNames() {
     //clear names div before adding new names
-    allNames.innerHTML = "";
-
-    //create and clear savedNAmes array before adding new names
-    let savedNames = [];
+    nameDiv.innerHTML = "";
 
     //split all the names by commas
-    let names = nameInput.value.split(",");
+    let names = nameInputField.value.split(",");
+
 
 
     //display all names below the button by creating new "name" divs
     for (name of names) {
-        const newName = document.createElement("div");
-        newName.classList.add("name");
-        newName.innerText = name;
-        allNames.appendChild(newName);
-        nameInput.value = "";
+        const newNameDiv = document.createElement("div");
+        newNameDiv.classList.add("name");
+        newNameDiv.innerText = name;
+        nameDiv.appendChild(newNameDiv);
+        nameInputField.value = "";
     }
 
-    //add typed names into an array and select one random name
-    savedNames.push(names);
-    let randomName = savedNames[Math.floor(Math.random() * savedNames.length)];
 
-
-    console.log(savedNames);
-
-
-
+    //choose one random winner and add different styling to it
+    let randomIndex = Math.floor(Math.random() * names.length);
+    document.querySelector(`.name div:nth-child(${randomIndex + 1})`)
+        .classList.add("selected")
+        ;
 }
 
 
-
-
-nameInput.addEventListener("keyup", function (event) {
+nameInputField.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         selectButton.click();
