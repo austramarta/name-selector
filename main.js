a88a85a822fd3b36259ee73cbb2714061f8157ca
austramarta <austramarta@users.noreply.github.com>
2020-11-24 13:33:38 +0200

    selecting random names

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const nameInput = document.getElementById("name-input");
 const allNames = document.getElementById("name");
 const selectButton = document.getElementById("select-name")
+// let savedNames = [];
 
 
 function enterNames() {
     //clear names div before adding new names
     allNames.innerHTML = "";
 
+    //create and clear savedNAmes array before adding new names
+    let savedNames = [];
+
     //split all the names by commas
     let names = nameInput.value.split(",");
 
-    //display all split names below the button by creating new "name" divs
+
+    //display all names below the button by creating new "name" divs
     for (name of names) {
         const newName = document.createElement("div");
         newName.classList.add("name");
@@ -19,8 +24,18 @@ function enterNames() {
         nameInput.value = "";
     }
 
+    //add typed names into an array and select one random name
+    savedNames.push(names);
+    let randomName = savedNames[Math.floor(Math.random() * savedNames.length)];
+
+
+    console.log(savedNames);
+
+
 
 }
+
+
 
 
 nameInput.addEventListener("keyup", function (event) {
@@ -29,3 +44,5 @@ nameInput.addEventListener("keyup", function (event) {
         selectButton.click();
     }
 });
+
+
